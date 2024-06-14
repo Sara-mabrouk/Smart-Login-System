93305b1f3e5d29bdb1dcbcacb7d7262690304500
Your Name <you@example.com>
2024-06-14 22:53:31 +0300

    Update welcome

--- js/welcome.js
+++ js/welcome.js
@@ -2,7 +2,7 @@ var logOutBtn = document.getElementById("logOutBtn");
 var welcomeUser = document.getElementById("userName");
 var userName = localStorage.getItem("userName");
 
-welcomeUser.innerHTML = "Welcome " + userName;
+welcomeUser.innerHTML = "حمدلله علي السلامه وقدر ولطف يـ " + userName;
 
 logOutBtn.addEventListener("click", function() {
     location.href = "index.html";
