b7f2c06b5c4ad5dc3f434829a162ee900a592957
Your Name <you@example.com>
2024-06-14 17:48:10 +0300

    Sign Up

---
+++ js/index.js
@@ -0,0 +1,44 @@
+var signupName = document.getElementById('signupName');
+var signupEmail = document.getElementById('signupEmail');
+var signupPass = document.getElementById('signupPass');
+var signupBtn = document.getElementById('signupBtn');
+var exitFelids = document.getElementById('exitFelids');
+var users = [];
+
+// localStorage ==>.
+if (localStorage.getItem("users") != null) {
+    users = JSON.parse(localStorage.getItem("users"));
+}
+
+// sign up ==>>
+function signUp() {
+    if (signupName.value == '' ||
+        signupEmail.value == '' ||
+        signupPass.value == '') {
+        exitFelids.innerHTML = `<span class"text-danger my-3"> Please Fill all Fields </span>`
+    } else {
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].email.toLowerCase() == signupEmail.value.toLowerCase()) {
+                exitFelids.innerHTML = `<span class"text-danger my-3">Email Already Exist  </span>`
+            }
+        }
+        getUserData()
+        exitFelids.innerHTML = `<span class"text-success my-3"> Sign up successful </span>`
+    }
+}
+
+function getUserData() {
+    var user = {
+        name: signupName.value,
+        email: signupEmail.value,
+        password: signupPass.value
+    }
+    users.push(user)
+    localStorage.setItem('users', JSON.stringify(users))
+    location.href = '/index.html';
+}
+
+// Event BTN ==>>
+signupBtn.addEventListener('click', function() {
+    signUp()
+});
