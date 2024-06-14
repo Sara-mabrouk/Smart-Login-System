7235f1c9a01bcb46e0d2a09805d928643a4c1935
Your Name <you@example.com>
2024-06-14 21:20:20 +0300

    Validation For Login

--- js/login.js
+++ js/login.js
@@ -11,24 +11,40 @@ if (localStorage.getItem("users") != null) {
     users = JSON.parse(localStorage.getItem("users"));
 }
 
+function isValidEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+function isValidPassword(password) {
+    return password.length >= 6;
+}
+
 function logIn() {
-    if (logEmail.value == '' ||
-        logPass.value == '') {
-        success.innerHTML = `<span class"text-danger my-3"> Please Fill all Fields </span>`
+    var email = logEmail.value;
+    var password = logPass.value;
+
+    if (email === '' || password === '') {
+        success.innerHTML = `<span class=" my-3">Please Fill all Fields</span>`;
+    } else if (!isValidEmail(email)) {
+        success.innerHTML = `<span class=" my-3">Please Enter a Valid Email</span>`;
+    } else if (!isValidPassword(password)) {
+        success.innerHTML = `<span class=" my-3">Password Must Be At Least 6 Characters</span>`;
     } else {
         for (let i = 0; i < users.length; i++) {
-            if (logEmail.value.toLowerCase() == users[i].email.toLowerCase() && logPass.value == users[i].password) {
-                success.innerHTML = `<span class"text-success my-3"> Success</span>`
+            if (email.toLowerCase() == users[i].email.toLowerCase() && password == users[i].password) {
+                success.innerHTML = `<span class=" my-3">Success</span>`;
 
-                localStorage.setItem('userName', JSON.stringify(users[i].name))
+                localStorage.setItem('userName', JSON.stringify(users[i].name));
                 location.href = "/home.html";
-                return
+                return;
             }
         }
-        success.innerHTML = `<span class"text-danger my-3"> Please Sign Up </span>`
+        success.innerHTML = `<span class="text-danger my-3">Please Sign Up</span>`;
     }
 }
+
 // Event BTN LogIn ==>>
 logBtn.addEventListener('click', function() {
-    logIn()
+    logIn();
 });
